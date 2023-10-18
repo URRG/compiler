c2a0e165d5a770a1049017ca7aa6b8dd01f1fa23
Utkarsh Rastogi <95937956+URRG@users.noreply.github.com>
2023-10-19 01:00:27 +0530

    Add files via upload

---
+++ c/a.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main(){
+    // Single line comment
+    
+    /* Multi-line comment
+       Like this */
+    
+    /* here */ int a; /* "int a" should be untouched */
+    
+	// This nested comment // This comment should be removed should be removed
+
+
+	
+	/* To make things /*  nested multi-line comment */ interesting */
+
+	return 0;
+}
